4e1be37ee663ddd457dc7bf755ba42274710580f
Sudip Paul <p452570@gmail.com>
2025-07-09 03:19:32 +0530

    tried something

--- src/app/api/analyze/route.ts
+++ src/app/api/analyze/route.ts
@@ -1,4 +1,4 @@
-import pdfParse from "pdf-parse";
+// import pdfParse from "pdf-parse";
 import mammoth from "mammoth";
 import gemini from "@/lib/gemini";
 
@@ -20,56 +20,57 @@ recommendations: <an optimized recommendations in bullet format, in bullet point
   const bytes = await resumeFile.arrayBuffer();
   const buffer = Buffer.from(bytes);
 
-  if (resumeFile.type === "application/pdf") {
-    const dataFromFile = await pdfParse(buffer);
-    const userMessage = `Here is the resume text: ${dataFromFile.text}`;
-    const response = await gemini.models.generateContent({
-      model: "gemini-2.0-flash",
-      contents: userMessage,
-      config: {
-        systemInstruction: aiRole,
-      },
-    });
-    if (!response.text) {
-      return new Response("Failed to analyze resume", { status: 500 });
-    }
-    console.log(response.text);
-    const score = response.text.match(/score\s*:\s*(\d+)/i)?.[1] || "";
+  // if (resumeFile.type === "application/pdf") {
+  //   const dataFromFile = await pdfParse(buffer);
+  //   const userMessage = `Here is the resume text: ${dataFromFile.text}`;
+  //   const response = await gemini.models.generateContent({
+  //     model: "gemini-2.0-flash",
+  //     contents: userMessage,
+  //     config: {
+  //       systemInstruction: aiRole,
+  //     },
+  //   });
+  //   if (!response.text) {
+  //     return new Response("Failed to analyze resume", { status: 500 });
+  //   }
+  //   console.log(response.text);
+  //   const score = response.text.match(/score\s*:\s*(\d+)/i)?.[1] || "";
 
-    const strongPointsBlock =
-      response.text.match(
-        /strongPoints\s*:\s*([\s\S]*?)weakPoints\s*:/i
-      )?.[1] || "";
+  //   const strongPointsBlock =
+  //     response.text.match(
+  //       /strongPoints\s*:\s*([\s\S]*?)weakPoints\s*:/i
+  //     )?.[1] || "";
 
-    const weakPointsBlock =
-      response.text.match(
-        /weakPoints\s*:\s*([\s\S]*?)recommendations\s*:/i
-      )?.[1] || "";
+  //   const weakPointsBlock =
+  //     response.text.match(
+  //       /weakPoints\s*:\s*([\s\S]*?)recommendations\s*:/i
+  //     )?.[1] || "";
 
-    const recommendationsBlock =
-      response.text.match(/recommendations\s*:\s*([\s\S]*)/i)?.[1] || "";
+  //   const recommendationsBlock =
+  //     response.text.match(/recommendations\s*:\s*([\s\S]*)/i)?.[1] || "";
 
-    // Matches lines starting with bullet points using • or *
-    const bulletRegex = /^[•*]\s+(.*)$/gm;
+  //   // Matches lines starting with bullet points using • or *
+  //   const bulletRegex = /^[•*]\s+(.*)$/gm;
 
-    function extractBullets(textBlock: string) {
-      const points = [];
-      let match;
-      while ((match = bulletRegex.exec(textBlock)) !== null) {
-        points.push(match[1].trim());
-      }
-      return points;
-    }
+  //   function extractBullets(textBlock: string) {
+  //     const points = [];
+  //     let match;
+  //     while ((match = bulletRegex.exec(textBlock)) !== null) {
+  //       points.push(match[1].trim());
+  //     }
+  //     return points;
+  //   }
 
-    const jsonResponse = {
-      score: parseInt(score),
-      strongPoints: extractBullets(strongPointsBlock),
-      weakPoints: extractBullets(weakPointsBlock),
-      recommendations: extractBullets(recommendationsBlock),
-    };
-    console.log(jsonResponse);
-    return new Response(JSON.stringify(jsonResponse), { status: 200 });
-  } else if (
+  //   const jsonResponse = {
+  //     score: parseInt(score),
+  //     strongPoints: extractBullets(strongPointsBlock),
+  //     weakPoints: extractBullets(weakPointsBlock),
+  //     recommendations: extractBullets(recommendationsBlock),
+  //   };
+  //   console.log(jsonResponse);
+  //   return new Response(JSON.stringify(jsonResponse), { status: 200 });
+  // } else 
+  if (
     resumeFile.type ===
     "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
   ) {
